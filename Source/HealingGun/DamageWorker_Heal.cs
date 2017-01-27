145b6e3774e1791ab2ada9dec2a37132e89b7831
erdelf <erdelf.productions@gmail.com>
2017-01-27 23:22:38 +0100

    Initial Upload

---
+++ Source/HealingGun/DamageWorker_Heal.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+
+namespace Verse
+{
+    public class DamageWorker_Heal : DamageWorker
+	{
+		public override float Apply(DamageInfo dinfo, Thing thing)
+		{
+			Pawn pawn = thing as Pawn;
+            if (pawn != null && pawn.Faction == RimWorld.Faction.OfPlayer)
+            {
+            	Log.Message("Starting healing on " + pawn.NameStringShort);
+            	//Log.Message("Analyzing Pawn");
+                Log.Message("Injury Count: " + pawn.health.hediffSet.GetHediffs<Hediff_Injury>().Count());
+                /*
+                foreach (Hediff_Injury current in pawn.health.hediffSet.GetHediffs<Hediff_Injury>())
+                {
+                    if ((current.IsNaturallyHealing() || current.NotNaturallyHealingBecauseNeedsTending()) && !current.IsOld()) // basically check for scars and old wounds
+                    {
+                        Log.Message("Healing " + current.def.defName + " on " + current.Part.def.defName);
+
+                        pawn.health.HealHediff(current, (int)current.Severity + 1);
+                    } else
+                    {
+                        Log.Message("Scars or old wounds can't be healed");
+                    }
+                }*/
+
+                int maxInjuries = 6;
+                int maxInjuriesPerBodypart;
+
+                foreach (BodyPartRecord rec in pawn.health.hediffSet.GetInjuredParts())
+                {
+                    if (maxInjuries > 0)
+                    {
+                        maxInjuriesPerBodypart = 2;
+                        foreach (Hediff_Injury current in from injury in pawn.health.hediffSet.GetHediffs<Hediff_Injury>() where injury.Part == rec select injury)
+                        {
+                            if (maxInjuriesPerBodypart > 0)
+                            {
+                                if (current.CanHealNaturally() && !current.IsOld()) // basically check for scars and old wounds
+                                {
+                                    Log.Message("Healing " + current.def.defName + " on " + current.Part.def.defName);
+                                    current.Heal((int)current.Severity + 1);
+                                    maxInjuries--;
+                                    maxInjuriesPerBodypart--;
+                                }
+                                else
+                                {
+                                    Log.Message("Scars or old wounds can't be healed");
+                                }
+                            }
+                        }
+                    }
+                }
+
+
+                Log.Message("Finished healing on " + pawn.NameStringShort);
+            }
+            return 0f;
+		}
+	}
+}
---
+++ Source/HealingGun/TargetCacheChanger.cs
@@ -0,0 +1,34 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using Verse.AI;
+
+namespace HealingGun
+{
+    [StaticConstructorOnStartup]
+    [HarmonyPatch(typeof(AttackTargetsCache))]
+    [HarmonyPatch("GetPotentialTargetsFor")]
+    static class TargetCacheChanger
+    {
+        static TargetCacheChanger()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("rimworld.erdelf.healing_gun");
+
+            harmony.PatchAll(typeof(TargetCacheChanger).Module);
+        }
+
+        static void Postfix(AttackTargetsCache _this, ref List<IAttackTarget> result, ref Thing th)
+        {
+            Pawn pawn = th as Pawn;
+            if(pawn!=null && pawn.equipment.Primary.def.Equals(ThingDef.Named("Healing_Gun")))
+            {
+                result.Clear();
+                result = th.Map.mapPawns.AllPawnsSpawned.Where((Pawn p) => !p.Dead && !p.Faction.HostileTo(pawn.Faction)).Cast<IAttackTarget>().ToList();
+            }
+        }
+    }
+}
