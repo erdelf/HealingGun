ee6824926ee39226277b69036caba10eead38b8e
erdelf <erdelf.productions@gmail.com>
2018-06-17 20:06:31 +0200

    Rimworld 1.0

--- Source/HealingGun/DamageWorker_Heal.cs
+++ Source/HealingGun/DamageWorker_Heal.cs
@@ -1,17 +1,17 @@
-﻿using System.Linq;
-using Verse;
-
-namespace HealingGun
+﻿namespace HealingGun
 {
+    using System.Linq;
+    using Verse;
+
     public class DamageWorker_Heal : DamageWorker
 	{
         public override DamageResult Apply(DamageInfo dinfo, Thing victim)
 		{
             if (victim is Pawn pawn)
             {
-                Log.Message("Starting healing on " + pawn.NameStringShort);
+                Log.Message(text: "Starting healing on " + pawn.Name.ToStringShort);
                 //Log.Message("Analyzing Pawn");
-                Log.Message("Injury Count: " + pawn.health.hediffSet.GetHediffs<Hediff_Injury>().Count());
+                Log.Message(text: "Injury Count: " + pawn.health.hediffSet.GetHediffs<Hediff_Injury>().Count());
                 /*
                 foreach (Hediff_Injury current in pawn.health.hediffSet.GetHediffs<Hediff_Injury>())
                 {
@@ -27,37 +27,32 @@ namespace HealingGun
                 }*/
 
                 int maxInjuries = 6;
-                int maxInjuriesPerBodypart;
 
                 foreach (BodyPartRecord rec in pawn.health.hediffSet.GetInjuredParts())
                 {
-                    if (maxInjuries > 0)
+                    if (maxInjuries <= 0) continue;
+                    int maxInjuriesPerBodypart = 2;
+                    foreach (Hediff_Injury current in from injury in pawn.health.hediffSet.GetHediffs<Hediff_Injury>() where injury.Part == rec select injury)
                     {
-                        maxInjuriesPerBodypart = 2;
-                        foreach (Hediff_Injury current in from injury in pawn.health.hediffSet.GetHediffs<Hediff_Injury>() where injury.Part == rec select injury)
+                        if (maxInjuriesPerBodypart <= 0) continue;
+                        if (current.CanHealNaturally() && !current.IsPermanent()) // basically check for scars and old wounds
                         {
-                            if (maxInjuriesPerBodypart > 0)
-                            {
-                                if (current.CanHealNaturally() && !current.IsOld()) // basically check for scars and old wounds
-                                {
-                                    Log.Message("Healing " + current.def.defName + " on " + current.Part.def.defName);
-                                    current.Heal((int)current.Severity + 1);
-                                    maxInjuries--;
-                                    maxInjuriesPerBodypart--;
-                                }
-                                else
-                                {
-                                    Log.Message("Scars or old wounds can't be healed");
-                                }
-                            }
+                            Log.Message(text: "Healing " + current.def.defName + " on " + current.Part.def.defName);
+                            current.Heal(amount: (int)current.Severity                  + 1);
+                            maxInjuries--;
+                            maxInjuriesPerBodypart--;
+                        }
+                        else
+                        {
+                            Log.Message(text: "Scars or old wounds can't be healed");
                         }
                     }
                 }
 
 
-                Log.Message("Finished healing on " + pawn.NameStringShort);
+                Log.Message(text: "Finished healing on " + pawn.Name.ToStringShort);
             }
-            return DamageResult.MakeNew();
+            return new DamageResult();
 		}
 	}
 }
--- Source/HealingGun/TargetChanger.cs
+++ Source/HealingGun/TargetChanger.cs
@@ -1,38 +1,34 @@
-﻿using Harmony;
-using RimWorld;
-using System.Collections.Generic;
-using System.Linq;
-using Verse;
-using Verse.AI;
-
-namespace HealingGun
+﻿namespace HealingGun
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using Harmony;
+    using RimWorld;
+    using Verse;
+    using Verse.AI;
+
     [StaticConstructorOnStartup]
-    static class TargetChanger
+    internal static class TargetChanger
     {
         static TargetChanger()
         {
-            HarmonyInstance harmony = HarmonyInstance.Create("rimworld.erdelf.healing_gun");
-            harmony.Patch(typeof(AttackTargetFinder).GetMethod("BestAttackTarget"), null, new HarmonyMethod(typeof(TargetChanger).GetMethod("TargetFinderPostfix")));
+            HarmonyInstance harmony = HarmonyInstance.Create(id: "rimworld.erdelf.healing_gun");
+            harmony.Patch(original: typeof(AttackTargetFinder).GetMethod(name: "BestAttackTarget"), prefix: null, postfix: new HarmonyMethod(method: typeof(TargetChanger).GetMethod(name: nameof(TargetFinderPostfix))));
         }
-
 
         public static void TargetFinderPostfix(ref Thing __result, Thing searcher, float maxDist)
         {
             Pawn pawn = searcher as Pawn;
-            if (pawn?.equipment?.Primary?.def?.Equals(ThingDef.Named("Healing_Gun")) ?? false)
-            {
-                IEnumerable<Pawn> pawns = searcher.Map.mapPawns.AllPawnsSpawned.Where((Pawn p) =>
-                                               !p.Dead && !p.Faction.HostileTo(pawn.Faction) && p != searcher && GenSight.LineOfSight(searcher.Position, p.Position, searcher.Map) && p.health.hediffSet.GetHediffs<Hediff_Injury>().Any(h=> h.CanHealNaturally() && !h.IsOld()));
+            if (!(pawn?.equipment?.Primary?.def?.Equals(obj: ThingDef.Named(defName: "Healing_Gun")) ?? false)) return;
+            IEnumerable<Pawn> pawns = searcher.Map.mapPawns.AllPawnsSpawned.Where(predicate: p =>
+                !p.Dead && !p.Faction.HostileTo(other: pawn.Faction) && p != searcher && GenSight.LineOfSight(start: searcher.Position, end: p.Position, map: searcher.Map) &&
+                p.health.hediffSet.GetHediffs<Hediff_Injury>().Any(predicate: h => h.CanHealNaturally() && !h.IsPermanent())).ToArray();
 
-                __result = GenClosest.ClosestThing_Global(searcher.Position, pawns.Where(p => p.RaceProps.Humanlike && p.Faction == searcher.Faction), maxDist);
-                if(__result==null)
-                    __result = GenClosest.ClosestThing_Global(searcher.Position, pawns.Where(p => p.Faction == searcher.Faction), maxDist);
-                if (__result==null)
-                __result = GenClosest.ClosestThing_Global(searcher.Position, pawns.Where(p => p.RaceProps.Humanlike), maxDist);
-                if(__result==null)
-                    __result = GenClosest.ClosestThing_Global(searcher.Position, pawns, maxDist);
-            }
+            __result =
+                ((GenClosest.ClosestThing_Global(center: searcher.Position, searchSet: pawns.Where(predicate: p => p.RaceProps.Humanlike && p.Faction == searcher.Faction), maxDistance: maxDist) ??
+                  GenClosest.ClosestThing_Global(center: searcher.Position, searchSet: pawns.Where(predicate: p => p.Faction == searcher.Faction),                          maxDistance: maxDist)) ??
+                 GenClosest.ClosestThing_Global(center: searcher.Position, searchSet: pawns.Where(predicate: p => p.RaceProps.Humanlike), maxDistance: maxDist)) ??
+                GenClosest.ClosestThing_Global(center: searcher.Position, searchSet: pawns, maxDistance: maxDist);
         }
     }
 }
